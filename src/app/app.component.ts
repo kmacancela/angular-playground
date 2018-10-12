abc4d21ddb5ccf6ea9e90f60169f0ec41b1a16ae
Karina Macancela <karymacancela@gmail.com>
2018-10-11 22:15:26 -0400

    Assignment 2 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
-  styles: [`
-  h3{
-    color: red
-  }`]
+  styleUrls: ['./app.component.css']
+  /*
+    styles: [`
+    h3{
+      color: red
+    }`] 
+  */
 })
 export class AppComponent {
+  username = '';
+  usernameCreationStatus = 'No username created yet.'
+
+  isUsernameEmpty(){
+    if(this.username === '') return true;
+    return false;
+  }
+
+  onCreateUsername(){
+    this.usernameCreationStatus = 'Username ' + this.username + ' was created.';
+    this.username = '';
+  }
 }
